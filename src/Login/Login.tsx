e514d8d9ee44099a55a4886d49809ded414b8c5c
andyWeb2022 <andy072892live@gmail.com>
2023-03-02 23:48:20 +0800

    Design the login page

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Login = () => {
-  return <div className="text-3xl font-bold underline">Login</div>;
+  return(
+    <div className="h-screen w-screen flex justify-center items-center bg-[#00324e]">
+      <button className="flex justify-center items-center h-[70px] w-[300px] bg-[#3397cf] text-[30px] text-[#ffffff] hover:bg-[#5ab0db]"><img src="/images/github-mark.png" className="w-[35px] w-[35px] mr-2 text-center"></img>Github Login</button>
+    </div>
+  )
+  
 };
 
 export default Login;
